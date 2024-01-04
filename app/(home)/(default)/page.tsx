89030641e1cedcbc89eff35ec7f6dd3ee9d00039
ChaeHyunIM <klqwrx7004@gmail.com>
2024-01-04 16:26:54 +0900

    unnecessary code and file removed

--- app/(home)/(default)/page.tsx
+++ app/(home)/(default)/page.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import {blogs as data} from '@/lib/data'
 import Posts from "@/components/Posts";
 import { getPostMetadata } from "@/lib/actions/blog";
 
 export default async function Home() {
-	// let blogs = data;
-	// let { data: blogs } = await readBlog();
 	const blogs = getPostMetadata();
 
 	return (
