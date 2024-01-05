94a513a0efe00983d2ba0f760a555f50471b444f
ChaeHyunIM <klqwrx7004@gmail.com>
2024-01-06 01:43:13 +0900

    nav bar renewaled
    - sticky nav bar
    - progress bar added for post detail page
    - nav bar title changes as the user scrolls down

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,61 +1,62 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-import { cn } from "@/lib/utils";
-import Navbar from "@/components/nav";
-import { Toaster } from "@/components/ui/toaster";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
+import { cn } from '@/lib/utils';
+import Navbar from '@/components/nav';
+import { Toaster } from '@/components/ui/toaster';
+import { FOOTER_DESCRIPTION } from '@/lib/constants';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	metadataBase: new URL("https://dailyblog-demo.vercel.app/"),
+  metadataBase: new URL('https://chaehyunim.vercel.app/'),
 
-	title: {
-		template: "%s | Daily Blog",
-		default: "Daily Blog",
-	},
-	authors: {
-		name: "chensokheng",
-	},
+  title: {
+    template: '%s | Chaehyun Space',
+    default: 'Chaehyun Space',
+  },
+  authors: {
+    name: 'chaehyunim',
+  },
 
-	description:
-		"Explore a world of captivating stories and insightful articles on our blog. From the latest trends to in-depth analyses, our blog covers a wide range of topics to keep you informed and entertained. Join our community of readers and discover thought-provoking content that sparks curiosity and fosters discussion. Stay updated with our diverse collection of blog posts, written by passionate contributors who share their expertise and unique perspectives. Engage with a platform that goes beyond the ordinary, providing you with enriching content that resonates with your interests.",
-	openGraph: {
-		title: "Daily Blog",
-		description:
-			"Explore a world of captivating stories and insightful articles on our blog. From the latest trends to in-depth analyses, our blog covers a wide range of topics to keep you informed and entertained. Join our community of readers and discover thought-provoking content that sparks curiosity and fosters discussion. Stay updated with our diverse collection of blog posts, written by passionate contributors who share their expertise and unique perspectives. Engage with a platform that goes beyond the ordinary, providing you with enriching content that resonates with your interests.",
-		url: "https://dailyblog-demo.vercel.app/",
-		siteName: "Daily Blog",
-		images: "/og.png",
-		type: "website",
-	},
-	keywords: ["daily web coding", "chensokheng", "dailywebcoding"],
+  description: FOOTER_DESCRIPTION,
+  openGraph: {
+    title: 'Chaehyun Space',
+    description: 'FOOTER_DESCRIPTION',
+    url: 'https://dailyblog-demo.vercel.app/',
+    siteName: 'Chaehyun Space',
+    images: '/images/cute_astronaut',
+    type: 'website',
+  },
+  keywords: [
+    'blog',
+    'chaehyunim',
+    'chaehyun space',
+    'chaehyun',
+    'chaehyun blog',
+    'chaehyunim blog',
+    'javascript',
+    'web frontend',
+    'React',
+    'Next.js',
+  ],
 };
 
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<body
-				className={cn("antialiased dark:bg-[#09090B]", inter.className)}
-			>
-				<ThemeProvider
-					attribute="class"
-					defaultTheme="dark"
-					enableSystem
-					disableTransitionOnChange
-				>
-					<main className="max-w-7xl mx-auto lg:py-10 space-y-10 p-5 lg:p-0">
-						<Navbar />
-						{children}
-					</main>
-				</ThemeProvider>
-				<Toaster />
-			</body>
-		</html>
-	);
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn('antialiased dark:bg-[#09090B]', inter.className)}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+          <div className="max-w-7xl mx-auto lg:py-6 space-y-6 p-5 lg:p-0">
+            <header className="w-full sticky top-0 z-10 bg-black bg-border-b-2">
+              <Navbar />
+            </header>
+            <main>{children}</main>
+          </div>
+        </ThemeProvider>
+        <Toaster />
+      </body>
+    </html>
+  );
 }
--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -3,37 +3,66 @@ import React from 'react';
 import HoverUnderLine from './HoverUnderLine';
 import Link from 'next/link';
 import Image from 'next/image';
-import profileImage from '@/public/profile.jpeg';
+import picture from '@/public/images/cute_astronaut.png';
 import { MENU, BLOG_TITLE } from '@/lib/constants';
+import { usePathname } from 'next/navigation';
+import { motion, useScroll } from 'framer-motion';
+import useScrollPosition from '@/hooks/useScrollPosition';
+
+type Props = {
+  isBlog?: boolean;
+};
 
 export default function Navbar() {
+  const pathname = usePathname();
+  const slug = pathname.split('/')[2];
+  const { scrollYProgress } = useScroll();
+  const scrollPosition = useScrollPosition();
+
+  const motionDivStyle = {
+    scaleX: scrollYProgress,
+    transformOrigin: '0%',
+  };
+  const isBlog = pathname.includes('/blog');
+
+  const getNavTitle = () => {
+    if (!isBlog) return BLOG_TITLE;
+    if (scrollPosition < 190) return BLOG_TITLE;
+    return slug;
+  };
+
   return (
-    <nav className="w-full justify-between items-center flex p-5 xl:p-0">
-      <div className="display: flex h-12 items-center gap-x-5">
-        <Image
-          src={profileImage}
-          alt={'chaehyun blog logo'}
-          width={48}
-          height={48}
-          className="rounded-full ring-2 ring-green-500 h-12 w-12"
-        />
-        <HoverUnderLine>
-          <Link href={'/'} className="font-bold text-3xl">
-            {BLOG_TITLE}
-          </Link>
-        </HoverUnderLine>
-      </div>
-      <div className="display: flex h-12 items-center gap-x-3">
-        {MENU.map(item => {
-          return (
-            <HoverUnderLine key={item.id}>
-              <Link href={item.href} className="font-bold text-2xl">
-                {item.title}
-              </Link>
-            </HoverUnderLine>
-          );
-        })}
-      </div>
-    </nav>
+    <>
+      <nav className="w-full justify-between items-center flex py-4 relative">
+        <div className="display: flex items-center gap-x-5">
+          <Image
+            src={picture}
+            alt={'chaehyun blog logo'}
+            width={48}
+            height={48}
+            className="rounded-full ring-2 ring-green-500"
+          />
+          <HoverUnderLine>
+            <Link href={'/'} className="font-bold text-2xl">
+              {getNavTitle()}
+            </Link>
+          </HoverUnderLine>
+        </div>
+        <div className="display: flex h-12 items-center gap-x-3">
+          {MENU.map(item => {
+            return (
+              <HoverUnderLine key={item.id}>
+                <Link href={item.href} className="font-bold text-xl">
+                  {item.title}
+                </Link>
+              </HoverUnderLine>
+            );
+          })}
+        </div>
+        {isBlog ? (
+          <motion.div className="absolute bottom-0 left-0 right-0 h-1 bg-green-500" style={motionDivStyle}></motion.div>
+        ) : null}
+      </nav>
+    </>
   );
 }
